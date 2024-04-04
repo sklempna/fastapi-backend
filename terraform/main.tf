42fb0c5ab7091118ff7949e55a0229b84b190873
sklempna <60576241+sklempna@users.noreply.github.com>
2024-04-04 17:20:23 +0200

    added basic terraform project

---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = var.cosmosdb_account_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover = false
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.example.location
+    failover_priority = 0
+  }
+}
+
+# ... (rest of the resources)provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = var.cosmosdb_account_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover = false
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.example.location
+    failover_priority = 0
+  }
+}
+
+# ... (rest of the resources)
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "The location of the resource group"
+  type        = string
+}
+
+variable "cosmosdb_account_name" {
+  description = "The name of the Cosmos DB account"
+  type        = string
+}
