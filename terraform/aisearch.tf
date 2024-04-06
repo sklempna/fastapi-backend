e7761c57f051ba9a4d04124d53278c9674dc00ae
sklempna <60576241+sklempna@users.noreply.github.com>
2024-04-06 13:43:14 +0200

    added ai search service to terraform

---
+++ terraform/aisearch.tf
@@ -0,0 +1,8 @@
+resource "azurerm_search_service" "search" {
+  name                = var.search_service_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = var.sku
+  replica_count       = var.replica_count
+  partition_count     = var.partition_count
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,3 +22,38 @@ variable "cosmosdb_container_name" {
   description = "The name of the Cosmos DB container"
   type        = string
 }
+
+variable "search_service_name" {
+  description = "The name of the Azure AI Search service"
+  type        = string
+}
+
+variable "search_sku" {
+  description = "The pricing tier of the search service you want to create (for example, basic or standard)."
+  default     = "standard"
+  type        = string
+  validation {
+    condition     = contains(["free", "basic", "standard", "standard2", "standard3", "storage_optimized_l1", "storage_optimized_l2"], var.sku)
+    error_message = "The sku must be one of the following values: free, basic, standard, standard2, standard3, storage_optimized_l1, storage_optimized_l2."
+  }
+}
+
+variable "search_replica_count" {
+  type        = number
+  description = "Replicas distribute search workloads across the service. You need at least two replicas to support high availability of query workloads (not applicable to the free tier)."
+  default     = 1
+  validation {
+    condition     = var.replica_count >= 1 && var.replica_count <= 12
+    error_message = "The replica_count must be between 1 and 12."
+  }
+}
+
+variable "search_partition_count" {
+  type        = number
+  description = "Partitions allow for scaling of document count as well as faster indexing by sharding your index over multiple search units."
+  default     = 1
+  validation {
+    condition     = contains([1, 2, 3, 4, 6, 12], var.partition_count)
+    error_message = "The partition_count must be one of the following values: 1, 2, 3, 4, 6, 12."
+  }
+}
