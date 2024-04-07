fe4ba8ba95e5595120ab2d5bcf983752fa4039f0
sklempna <60576241+sklempna@users.noreply.github.com>
2024-04-07 15:47:23 +0200

    fixed typos

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -43,7 +43,7 @@ variable "search_replica_count" {
   description = "Replicas distribute search workloads across the service. You need at least two replicas to support high availability of query workloads (not applicable to the free tier)."
   default     = 1
   validation {
-    condition     = var.replica_count >= 1 && var.replica_count <= 12
+    condition     = var.search_replica_count >= 1 && var.search_replica_count <= 12
     error_message = "The replica_count must be between 1 and 12."
   }
 }
@@ -53,7 +53,7 @@ variable "search_partition_count" {
   description = "Partitions allow for scaling of document count as well as faster indexing by sharding your index over multiple search units."
   default     = 1
   validation {
-    condition     = contains([1, 2, 3, 4, 6, 12], var.partition_count)
+    condition     = contains([1, 2, 3, 4, 6, 12], var.search_partition_count)
     error_message = "The partition_count must be one of the following values: 1, 2, 3, 4, 6, 12."
   }
 }
