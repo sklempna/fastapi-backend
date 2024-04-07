4872424682b146261177355e50d09c8d1e3666f7
sklempna <60576241+sklempna@users.noreply.github.com>
2024-04-07 15:50:51 +0200

    typos

--- terraform/aisearch.tf
+++ terraform/aisearch.tf
@@ -2,7 +2,7 @@ resource "azurerm_search_service" "search" {
   name                = var.search_service_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = var.sku
-  replica_count       = var.replica_count
-  partition_count     = var.partition_count
+  sku                 = var.search_skusku
+  replica_count       = var.search_replica_count
+  partition_count     = var.search_partition_count
 }
